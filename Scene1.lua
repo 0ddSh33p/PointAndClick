303a4428c39de54696a178551f7c4b96923db1e9
lonelyfloat <cephhead_prsnl@protonmail.com>
2024-09-22 15:51:21 -0400

    Implemented interaction + screen scaling

--- Scene1.lua
+++ Scene1.lua
@@ -8,31 +8,47 @@ function M.load()
 
     local doorimg = love.graphics.newImage("/arts/LeftDoorA.png")
     local plantNormal = love.graphics.newImage("/arts/plantA.png")
+    local bgimg = love.graphics.newImage("/arts/RoomA.png")
 
     M.data = {
         {
             name = "bg",
-            img = love.graphics.newImage("/arts/RoomA.png"),
+            img = bgimg,
+            hitbox = {
             x = 0,
             y = 0,
+            w = bgimg:getWidth(),
+            h = bgimg:getHeight()
+            },
             sx = 1,
             sy = 1
         },
         {
             name = "leftDoor",
             img = doorimg,
+            hitbox = {
             x = 15,
-            y = height - doorimg:getHeight() - 20,
+            y = height - doorimg:getHeight() - 385,
+            w = doorimg:getWidth(),
+            h = doorimg:getHeight()
+            },
             sx = 1,
             sy = 1
         },
         {
             name = "plant",
             img = plantNormal,
+            hitbox = {
             x = 1200,
             y = 780,
+            w = plantNormal:getWidth(),
+            h = plantNormal:getHeight()
+            },
             sx = 1,
-            sy = 1
+            sy = 1,
+            interact = function(self)
+                self.hitbox.x = self.hitbox.x + 50
+            end
         },
     }
 end
--- main.lua
+++ main.lua
@@ -2,17 +2,27 @@ local cursorSizex = 0.1
 local cursorSizey = 0.1
 local cursor = nil
 local scaler = 0.01
+local utils = require("utils")
+local cursorDimensions = { w = 0, h = 0 }
+local cursorHitbox = { x = 0, y = 0, w = 0, h = 0}
 local scene = require("Scene1")
-
-
 local minCursorSize = 0.01
 local maxCursorSize = 1.0
+
+local resolutionX, resolutionY = 1920,1080
+
+local resRatioX, resRatioY = 1920,1080
 
 function love.load()
     love.window.setFullscreen(true, "desktop")
     cursor = love.graphics.newImage("/arts/pointer.png")
     cursorSizex = 0.1
     cursorSizey = 0.1
+    cursorDimensions.w = cursor:getWidth()
+    cursorDimensions.h = cursor:getHeight()
+    local dimensionX, dimensionY = love.graphics.getDimensions()
+    resRatioX = dimensionX/resolutionX
+    resRatioY = dimensionY/resolutionY
     scene.load()
 
     love.mouse.setVisible(false)
@@ -47,20 +57,29 @@ function love.wheelmoved(x, y)
     if cursorSizey > maxCursorSize then
         cursorSizey = maxCursorSize
     end
+
+    cursorHitbox.w = cursorDimensions.w * cursorSizex
+    cursorHitbox.h = cursorDimensions.h * cursorSizey
 end
 
-function love.update(dt)
-
+function love.mousepressed(x, y, button)
+    if button == 1 then -- left mouse button
+        cursorHitbox.x = x
+        cursorHitbox.y = y
+        for _, content in pairs(scene.data) do
+            if utils.aabbColliding(cursorHitbox, content.hitbox) then
+                if content.interact then content:interact() end
+            end
+        end
+    end
 end
 
 
 function love.draw()
     love.graphics.setBackgroundColor(0, 0, 0, 1)
-
-    local a = 1
     -- changed keys to indicies because the draw order was randomized with the way it was before
     for _, content in pairs(scene.data) do
-        love.graphics.draw(content.img, content.x, content.y, 0, content.sx, content.sy)
+        love.graphics.draw(content.img, content.hitbox.x * resRatioX, content.hitbox.y * resRatioY, 0, content.sx*resRatioX, content.sy*resRatioY)
     end
 
     love.graphics.draw(cursor, love.mouse.getX(), love.mouse.getY(), 0, cursorSizex, cursorSizey)
